c6a4078b5f81ccff8c5a925307b11aec1391bd17
bachuanush <anushrao.326@gmail.com>
2025-08-23 10:24:54 +0530

    c

---
+++ Hospital_Mngmt/src/Hosp_JDBC/LoginService.java
@@ -0,0 +1,33 @@
+package Hosp_JDBC;
+
+import java.sql.*;
+
+public class LoginService {
+	 public static boolean login(String username, String password) { 
+
+	        try (Connection con = DBConnection.getConnection()) { 
+
+	            String query = "SELECT * FROM staff WHERE username = ? AND password = ?"; 
+
+	            PreparedStatement ps = con.prepareStatement(query); 
+
+	            ps.setString(1, username); 
+
+	            ps.setString(2, password); 
+
+	 
+
+	            ResultSet rs = ps.executeQuery(); 
+
+	            return rs.next(); // login success if a match is found 
+
+	        } catch (Exception e) { 
+
+	            e.printStackTrace(); 
+
+	            return false; 
+
+	        } 
+
+	    } 
+}
